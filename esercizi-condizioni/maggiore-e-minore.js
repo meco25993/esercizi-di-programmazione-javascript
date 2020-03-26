06b52284eda892e345814e4176799b8d4e6aed75
meco25993 <tiger.25@hotmail.it>
2020-03-26 17:58:57 +0100

    prova

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -3,3 +3,36 @@
   Scrivi un programma che prenda quattro numeri in input, stampi in output il maggiore e il minore.
   http://www.imparareaprogrammare.it
 */
+
+var uno = 5;
+var due = 4;
+var tre = 7;
+var qua = 7;
+
+if (uno > due && uno > tre && uno > qua) {
+  console.log ('la variabile uno è maggiore di tutte')
+}
+else if (uno < due && uno < tre && uno < qua) {
+  console.log ('la variabile uno è minore di tutte')
+}
+
+if (due > uno && due > tre && due > qua) {
+  console.log ('la variabile due è maggiore di tutte')
+}
+else if (due < uno && due < tre && due < qua) {
+  console.log ('la variabile due è minore di tutte')
+}
+
+if (tre > uno && tre > due && tre > qua) {
+  console.log ('la variabile tre è maggiore di tutte')
+}
+else if (tre < uno && tre < due && tre < qua) {
+  console.log ('la variabile tre è minore di tutte')
+}
+
+if (qua > uno && qua > tre && qua > due) {
+  console.log ('la variabile quattro è maggiore di tutte')
+}
+else if (qua < uno && qua < tre && qua > due) {
+  console.log ('la variabile quattro è minore di tutte')
+}
--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -3,3 +3,10 @@
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
 */
+
+var n = 57;
+
+if ( n%2 == 0 )
+console.log ('Il numero inserito è pari quindi restituisco 0');
+else
+  console.log ('Il numero inserito è dispari quindi restituisco 1');
